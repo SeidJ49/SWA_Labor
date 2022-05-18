b028cdee46c5cddfd12efa7a4b2e3921317315bb
finnguist <finn.guist@gmail.com>
2022-05-18 09:38:44 +0200

    Entity_classes

---
+++ SwA_Labor/src/main/java/entity_classes/service_contract.java
@@ -0,0 +1,57 @@
+package entity_classes;
+
+import javax.persistece.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "SERVICE_CONTRACT")
+public class service_contract {
+
+    @Id
+    @SequenceGenerator(name = "depSeq", sequenceName = "ZSEQ_DEP_ID", allocationSize = 1, initialValue = 10)
+    @GeneratedValue(generator = "depSeq")
+    
+    @Column(name = "id")
+    private Long id;
+
+
+    @Column(name = "depname", length=64, unique = true)
+    private String depname;
+    
+// 	The following two lines are optional
+//  @OneToMany(mappedBy="department") // department: Attribute in Person class
+//  private List<Person> persons;
+
+    public Department() {
+    }
+
+    public Department(String depname) {
+        this.depname = depname;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDepname() {
+        return depname;
+    }
+
+    public void setDepname(String depname) {
+        this.depname = depname;
+    }
+    
+//    public List<Person> getPersons() {
+//    	return persons;
+//    }
+
+}
